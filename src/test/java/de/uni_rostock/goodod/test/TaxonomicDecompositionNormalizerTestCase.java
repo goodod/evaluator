1668927be77f0eb1aff13ccb16553a2f5d634fcf
Niels Grewe <niels.grewe@uni-rostock.de>
2012-02-17 16:25:03 +0000

    Unit test for the taxonomic decomposition normalizer.
    git-svn-id: http://www.greeninghealthcare.org/repository/goodod/trunk/sources/evaluator@680 92d0aff3-7ff0-bf43-a321-9b83dfe78b24

---
+++ src/test/java/de/uni_rostock/goodod/test/TaxonomicDecompositionNormalizerTestCase.java
@@ -0,0 +1,98 @@
+/**
+  Copyright (C) 2012 The University of Rostock.
+ 
+  Written by:  Niels Grewe
+  Created: 17.02.2012
+  
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  You should have received a copy of the GNU General Public
+  License along with this program; see the file COPYING.
+  If not, write to the Free Software Foundation,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+ */
+package de.uni_rostock.goodod.test;
+
+import org.junit.*;
+import org.semanticweb.owlapi.model.*;
+
+import de.uni_rostock.goodod.owl.TaxonomicDecompositionNormalizer;
+
+/**
+ * @author Niels Grewe
+ *
+ */
+public class TaxonomicDecompositionNormalizerTestCase extends
+		AbstractNormalizerTestCase {
+	@Override
+	@Before public void setUp() throws OWLOntologyCreationException
+	{
+		super.setUp();
+		normalizer = new TaxonomicDecompositionNormalizer();
+	}
+	
+	// We only need one test because we know check taxonomic decomposition works in another test.
+	@Test public void testTaxonomicDecomposition() throws OWLOntologyCreationException
+	{
+		OWLClass A = addClass("A");
+		OWLClass B = addClass("B");
+		OWLClass C = addClass("C");
+		OWLObjectProperty P = factory.getOWLObjectProperty(IRI("P"));
+		OWLClassExpression AAndB = factory.getOWLObjectIntersectionOf(A, B);
+		OWLClassExpression PSomeAAndB = factory.getOWLObjectSomeValuesFrom(P, AAndB);
+		OWLClassExpression PSomeA = factory.getOWLObjectSomeValuesFrom(P, B);
+		OWLClassExpression PSomeB = factory.getOWLObjectSomeValuesFrom(P, B);
+		OWLAxiom PDecl = factory.getOWLDeclarationAxiom(P);
+		manager.addAxiom(ontology,PDecl);
+		addSubClassOf(A,C);
+		OWLAxiom CEquivPSomeAAndB = factory.getOWLEquivalentClassesAxiom(C,PSomeAAndB);
+		manager.addAxiom(ontology,CEquivPSomeAAndB);
+		normalizer.normalize(ontology);
+		
+		boolean hasPSomeAEquiv = false;
+		boolean hasPSomeBEquiv = false;
+		for (OWLEquivalentClassesAxiom eq : ontology.getAxioms(AxiomType.EQUIVALENT_CLASSES))
+		{
+			if (false == hasPSomeAEquiv)
+			{
+				if (eq.contains(PSomeA))
+				{
+					hasPSomeAEquiv = true;
+					for (OWLClassExpression ce : eq.getClassExpressionsMinus(PSomeA))
+					{
+						if (ce instanceof OWLClass)
+						{
+							// Check that we have declaration axioms for all of the named classes.
+							assertTrue(ontology.containsAxiom(factory.getOWLDeclarationAxiom(ce.asOWLClass())));
+						}
+					}
+				}
+			}
+			
+			if (false == hasPSomeBEquiv)
+			{
+				if (eq.contains(PSomeB))
+				{
+					hasPSomeBEquiv = true;
+					for (OWLClassExpression ce : eq.getClassExpressionsMinus(PSomeA))
+					{
+						if (ce instanceof OWLClass)
+						{
+							// Check that we have declaration axioms for all of the named classes.
+							assertTrue(ontology.containsAxiom(factory.getOWLDeclarationAxiom(ce.asOWLClass())));
+						}
+					}
+				}
+			}
+			if (hasPSomeBEquiv && hasPSomeAEquiv)
+			{
+				break;
+			}
+		}
+		assertTrue(hasPSomeBEquiv && hasPSomeAEquiv);
+	}
+}
